14217179ab3d1ce8876a0f9a1cbd57649596b126
Ricardo <ricardo@themoondev.com>
2020-07-02 18:28:37 -0600

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import '../App.css';
 import { DISHES } from '../shared/dishes';
 import DishDetail from './DishdetailComponent'
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component{
 
@@ -12,42 +14,29 @@ class Main extends Component{
         super(props);
 
         this.state = {
-        dishes: DISHES,
-        selectedDish: null
+            dishes: DISHES
         };
-    }
-
-    onDishSelect(dishId) {
-        this.setState({ selectedDish:dishId })
-    }
-
-    renderDish(selectedDish) {
-        if (selectedDish != null) {
-            return (
-                <DishDetail 
-                    // The filter function returns an array with all the elements that
-                    // satisfy the given condition, but since we only need 1 element
-                    // and we are sure that that the dishId is unique we retrieve 
-                    // the first element of the array.
-                    dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-            );    
-        }
-        else {
-            return (
-                <div>
-
-                </div>
-            );
-        }
     }
   
     render() {
+
+        const HomePage = () => {
+            return(
+                <Home />
+            );
+        }
+        
         return (
             <div>
                 <Header />
-                <Menu dishes={this.state.dishes} 
-                    onClick={(dishId) => this.onDishSelect(dishId)}/>
-                {this.renderDish(this.state.selectedDish)}
+                    <Switch>
+                        <Route path="/home" component={HomePage} />
+
+                        {/* We pass the Menu component as the return value of an arrow function 
+                        because there are props to be passed into it. */}
+                        <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/> } />
+                        <Redirect to="/home" />
+                    </Switch>
                 <Footer />
             </div>
         );
